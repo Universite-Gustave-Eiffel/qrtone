21db070714478f58e3695029d27e93a9dda3fae4
nicolas-f <fortin@ad.ifsttar.fr>
2018-03-06 14:08:44 +0100

    Implement generalized goertzel

--- src/main/c/warble.c
+++ src/main/c/warble.c
@@ -1,7 +1,89 @@
-
+/*
+* BSD 3-Clause License
+*
+* Copyright (c) 2018, Ifsttar
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*  Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+*  Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+*  Neither the name of the copyright holder nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
 
 #include "warble.h"
 #include "math.h"
+
+/**
+ * http://asp.eurasipjournals.com/content/pdf/1687-6180-2012-56.pdf
+ * ipfs://QmdAMfyq71Fm72Rt5u1qtWM7teReGAHmceAtDN5SG4Pt22
+ * Sysel and Rajmic:Goertzel algorithm generalized to non-integer multiples of fundamental frequency. EURASIP Journal on Advances in Signal Processing 2012 2012:56.
+ * @param signal Audio signal
+ * @param s_length Audio signal array size
+ * @param sampleRate Sampling rate in Hz
+ * @param freqs Array of frequency search in Hz
+ * @param f_length Size of freqs
+ * @param[out] outFreqsPower Rms power, must be allocated with the same size of freqs
+ */
+void generalized_goertzel(const double* signal, int32_t s_length,double sampleRate,const double* freqs, int32_t f_length, double* outFreqsPower) {
+	int32_t id_freq;
+	double* y_real = malloc(sizeof(double) * f_length);
+	double* y_im = malloc(sizeof(double) * f_length);
+	// Fix frequency using the sampleRate of the signal
+	double samplingRateFactor = s_length / sampleRate;
+	// Computation via second-order system
+	for(id_freq = 0; id_freq < f_length; id_freq++) {
+		// for a single frequency :
+		// precompute the constants
+		double pik_term = 2 * M_PI *(freqs[id_freq] * samplingRateFactor) / (s_length);
+		double cc_real = cos(pik_term);
+		double cos_pik_term2 = cc_real * 2;
+		double cc_im = -sin(pik_term);
+		// state variables
+		double s0 = 0.;
+		double s1 = 0.;
+		double s2 = 0.;
+		int32_t ind;
+		// 'main' loop
+		// number of iterations is (by one) less than the length of signal
+		for(ind=0; ind < s_length - 1; ind++) {
+			s0 = signal[ind] + cos_pik_term2 * s1 - s2;
+			s2 = s1;
+			s1 = s0;
+		}
+		// final computations
+		s0 = signal[s_length - 1] + cos_pik_term2 * s1 - s2;
+
+		// complex multiplication substituting the last iteration
+		// and correcting the phase for (potentially) non - integer valued
+		// frequencies at the same time
+		y_real[id_freq] = (s0 - s1 * cc_real) * cos(pik_term * (s_length - 1));
+		y_im[id_freq] = (s0 - s1 * cc_im) * -sin(pik_term * (s_length - 1));	
+		// Compute RMS
+		outFreqsPower[id_freq] = sqrt(y_real[id_freq] * y_real[id_freq] + y_im[id_freq] * y_im[id_freq]) / s_length;
+	}
+}
+
 
 
 warble* warble_create(double firstFrequency,
