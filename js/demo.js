d6c4712c7488c95cace9f21c6f19d025e84a253f
nicolas-f <[omitted]>
2020-07-31 11:02:00 +0200

    display option as control

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,19 @@
+function getFECLevel() {
+    // forward correction error level (0=low 3=max)
+    for(const x of Array(4).keys()) {
+        if(document.getElementById("radio-quality-"+x).checked) {
+            return x;
+        }
+    }
+}
+
+function getCRC() {
+    // add 16 bits crc if != 0
+    return document.getElementById("radio-crc-on").checked;
+}
+
 function sendChat() {
     // Convert input to int array
-    var ecc_level = 2; // forward correction error level (0=low 3=max)
-    var addCRC = 1; // add 16 bits crc if != 0
     const encoder = new TextEncoder()
     var user = Array.from(encoder.encode($('input[name=fname]').val()).values());
     var message = Array.from(encoder.encode($('textarea[name=fmessage]').val()).values());
@@ -12,13 +24,13 @@ function sendChat() {
         tx.destroy();
     }
     var tx = QRTone.transmitter({onFinish: function () { console.log("transmission complete"); }});
-    tx.transmit(bytes, ecc_level, addCRC);
+    tx.transmit(bytes, getFECLevel(), getCRC());
 }
 
 function sendColor() {
     // Send data
     var tx = QRTone.transmitter({onFinish: function () { console.log("transmission complete"); }});
-    tx.transmit([colorPicker.color.rgb.r, colorPicker.color.rgb.g, colorPicker.color.rgb.b]);
+    tx.transmit([colorPicker.color.rgb.r, colorPicker.color.rgb.g, colorPicker.color.rgb.b], getFECLevel(), getCRC());
 }
 
 function onNewMessage(payload, epoch) {
@@ -69,3 +81,8 @@ $( "#tabs" ).tabs({ active: 0 });
 $( function() {
 $( "input" ).filter(":radio").checkboxradio();
 } );
+function resetMsg() {
+    $("#receivedMessage")[0].value = "Click on \"Mic On\" in order to listen for messages.\n";
+}
+window.setTimeout(resetMsg, 500);
+
